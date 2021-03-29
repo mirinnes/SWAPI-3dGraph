e1f7bc2286bd9cc35615a44444837e4168bd0782
Miranda Lopez <mirandaineslopez@gmail.com>
2021-03-29 18:46:12 -0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ForceGraph3D from 'react-force-graph-3d';
+import './app.css';
+import { useQuery, gql } from '@apollo/client';
+const ALL_PLANETS = gql`
+	{
+		allPlanets {
+			planets {
+				name
+				filmConnection {
+					films {
+						title
+					}
+				}
+			}
+		}
+		allFilms {
+			films {
+				title
+			}
+		}
+	}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [displaySideBar, setdisplaySideBar] = useState(false);
+	const fetched = useQuery(ALL_PLANETS);
+	let gphData = { nodes: [], links: [] };
+	if (fetched.data) {
+		gphData = constructNodesAndLinks(fetched.data);
+	}
+	const getHTMLsideBar = (onClose) => {
+		return (
+			<div className='sideBar'>
+				<button onClick={onClose}>Close</button>
+				<h1>DESCRIPCION</h1>
+			</div>
+		);
+	};
+
+	return (
+		<>
+			<ForceGraph3D
+				graphData={gphData}
+				backgroundColor='#29A9E0'
+				width={displaySideBar ? window.innerWidth / 2 : window.innerWidth}
+				nodeResolution={10}
+				nodeLabel={(node) =>
+					node.type === 'planet'
+						? `<h3 class="planet-label">${node.label.toUpperCase()}</h3>`
+						: `<h3 class="film-label">${node.label.toUpperCase()}</h3>`
+				}
+				onNodeHover={(node) =>
+					(document.getElementById('root').style.cursor = node
+						? 'pointer'
+						: null)
+				}
+				nodeColor={(node) =>
+					node.type === 'film' ? 'white' : node.films.length ? 'black' : 'blue'
+				}
+				onNodeClick={() => setdisplaySideBar(!displaySideBar)}
+			/>
+			{displaySideBar &&
+				getHTMLsideBar(() => setdisplaySideBar(!displaySideBar))}
+		</>
+	);
 }
 
 export default App;
+
+function constructNodesAndLinks(data) {
+	const filmsTitles = getAllFilmsTitlesHelper(data.allFilms);
+	const planetsArray = getAllPlanetsNamessHelper(data.allPlanets);
+	const planetWithNoFilmNodes = splitPlanetsWithNoMovies(planetsArray);
+	const planetsWithFilmsNodes = splitPlanetsWithMovies(planetsArray);
+	const planetsWithFilmsLinks = setPlanetsWithFilmsLinksHelper(
+		planetsWithFilmsNodes,
+		filmsTitles
+	);
+	const planetWithNoFilmLinks = setPlanetsWithNoFilmLinksHelper(
+		planetWithNoFilmNodes
+	);
+	let arrayNodes = [
+		...planetsWithFilmsNodes,
+		planetWithNoFilmNodes,
+		filmsTitles,
+	].flat();
+	let arrayLinks = [...planetsWithFilmsLinks, planetWithNoFilmLinks].flat();
+
+	return { nodes: arrayNodes, links: arrayLinks };
+}
+
+function getAllFilmsTitlesHelper(allFilms) {
+	return allFilms.films.map((film, i) => {
+		return { id: 'film' + i, label: film.title, type: 'film' };
+	});
+}
+function getAllPlanetsNamessHelper(allPlanets) {
+	return allPlanets.planets.map((planet, i) => {
+		return {
+			id: 'planet' + i,
+			label: planet.name,
+			type: 'planet',
+			films: planet.filmConnection.films.map((film) => film.title),
+		};
+	});
+}
+function splitPlanetsWithNoMovies(planets) {
+	return planets.filter((planet) => planet.films.length === 0);
+}
+function splitPlanetsWithMovies(planets) {
+	return planets.filter((planet) => planet.films.length !== 0);
+}
+function setPlanetsWithNoFilmLinksHelper(planetsWithNoFilm) {
+	//console.log(`planetsWithNoFilm`, planetsWithNoFilm);
+	let links = planetsWithNoFilm.map((planet, i) => {
+		let planetLinks = [];
+		for (let j = i + 1; j < planetsWithNoFilm.length; j++) {
+			planetLinks.push({ source: planet.id, target: planetsWithNoFilm[j].id });
+		}
+		return planetLinks.flat();
+	});
+	return links.flat();
+}
+
+function setPlanetsWithFilmsLinksHelper(planetsWithFilms, films) {
+	const getFilmId = (filmName) => {
+		let indexFilm = films.findIndex((filmItem) => filmItem.label === filmName);
+		return films[indexFilm].id;
+	};
+
+	let links = planetsWithFilms.map((planet) => {
+		return planet.films.map((filmConnected) => {
+			return { source: planet.id, target: getFilmId(filmConnected) };
+		});
+	});
+	return links.flat();
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+	uri: 'https://swapi.apis.guru',
+	cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
